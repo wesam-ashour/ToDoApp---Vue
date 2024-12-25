a27fb30f499bac55debac71ee75bb7fd0e8097d7
Eng.Wesam Ashour <wesam-ashour@outlook.com>
2024-12-25 12:01:34 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <script setup>
-import { ref } from 'vue'
+import {ref} from 'vue'
 
 const header = ref('Shopping List App')
+const editing = ref(false)
 const items = ref([
-    { id: 1, label: "10 party hats" },
-    { id: 2, label: "2 board games" },
-    { id: 3, label: "20 cups" }
+    {id: 1, label: "10 party hats"},
+    {id: 2, label: "2 board games"},
+    {id: 3, label: "20 cups"}
 ])
 const newItem = ref("")
 const newItemHighPriority = ref(false)
+
+const saveItem = () => {
+    items.value.push({id: items.value.length + 1, label: newItem.value});
+    newItem.value = "";
+}
+
+const doEdit = (e) => {
+    editing.value = e;
+    newItem.value = ""
+}
 </script>
 
 <template>
-    <h1>{{ header }}</h1>
-    <form class="add-item-form" @submit.prevent="items.push({ id: items.length + 1, label: newItem })">
+    <div class="header">
+        <h1>{{ header }}</h1>
+        <button v-if="editing" class="btn" @click="doEdit(false)">cancel</button>
+        <button v-else class="btn btn-primary" @click="doEdit(true)">Add item</button>
+    </div>
+
+    <form class="add-item-form"
+          v-if="editing"
+          @submit.prevent="saveItem">
         <input v-model.trim="newItem"
                type="text" placeholder="Add an item">
-        <label>
+        <label style="color: red">
             <input type="checkbox" v-model="newItemHighPriority">
             High Priority
         </label>
-        <button class="btn btn-primary">
+        <button :disabled="newItem.length === 0" class="btn btn-primary">
             Save Item
         </button>
     </form>
@@ -29,6 +47,7 @@ const newItemHighPriority = ref(false)
             {{ label }}
         </li>
     </ul>
+    <p v-if="!items.length">There is no items!</p>
 </template>
 
 <style scoped>

c552a3d93eb9dd3eff8aa8cfab36b9527401e4a8
Eng.Wesam Ashour <wesam-ashour@outlook.com>
2024-12-25 11:01:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+const header = ref('Shopping List App')
+const items = ref([
+    { id: 1, label: "10 party hats" },
+    { id: 2, label: "2 board games" },
+    { id: 3, label: "20 cups" }
+])
+const newItem = ref("")
+const newItemHighPriority = ref(false)
+</script>
+
+<template>
+    <h1>{{ header }}</h1>
+    <form class="add-item-form" @submit.prevent="items.push({ id: items.length + 1, label: newItem })">
+        <input v-model.trim="newItem"
+               type="text" placeholder="Add an item">
+        <label>
+            <input type="checkbox" v-model="newItemHighPriority">
+            High Priority
+        </label>
+        <button class="btn btn-primary">
+            Save Item
+        </button>
+    </form>
+    <ul>
+        <li v-for="({ id, label }, index) in items" :key="id">
+            {{ label }}
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
